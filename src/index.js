8ff0f1afb4b0525b517d7daa31e29ddf7d07bdd4
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-11-07 09:50:05 +0700

    Generating Random Values for Our Page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-    <Detail />,
+    <Detail message="This is coming from props!" />,
     document.getElementById('app')
 );

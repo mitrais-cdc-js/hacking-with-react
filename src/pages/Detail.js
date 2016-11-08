fc6c3e6ce6bbcb24564c7e1e2e5764fc30828cc2
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-11-08 14:03:59 +0700

    Fetching Ajax Data from GitHub using SuperAgent

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ajax from 'superagent';
 
 class Detail extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = { commits: [] };
+    }
+
+    componentWillMount() {
+        ajax.get('https://api.github.com/repos/facebook/react/commits')
+            .end((error, response) => {
+                if (!error && response) {
+                    this.setState({ commits: response.body });
+                } else {
+                    console.log('There was an error fetching from GitHub', error);
+                }
+            });
     }
 
     render() {

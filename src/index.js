533b73c87311e655c5a74d1c02998e09344d5d4e
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-11-09 09:31:58 +0700

    Introducing React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-    <Detail />,
+    <Router history={hashHistory}>
+        <Route path="/" component={Detail} />
+    </Router>,
     document.getElementById('app')
 );
